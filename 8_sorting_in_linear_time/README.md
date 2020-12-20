b8baef12485bef73e16bc066e9c0e0aae1ddea73
Ray Tien <a0939552854@gmail.com>
2020-12-20 14:22:51 +0800

    Implemeting counting sort.

---
+++ 8_sorting_in_linear_time/README.md
@@ -0,0 +1,5 @@
+# Sorting in linear time
+
+## Counting sort
+
+- sorting technique based on keys between a specific range. It works by counting the number of objects having distinct key values (kind of hashing). Then doing some arithmetic to calculate the position of each object in the output sequence.
---
+++ 8_sorting_in_linear_time/countingSort.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def countSort(arr, min, max):
+    
+    # Init
+    count_dict = {}
+    cumulativeSum_dict = {}
+    sorted = []
+    for i in range(min,max+1):
+        count_dict[i] = 0
+        cumulativeSum_dict[i] = 0
+
+    for index, num in enumerate(arr):
+        if num in count_dict.keys():
+            count_dict[num] +=1
+        else:
+            count_dict[num] = 1
+    
+    sum = 0
+    for i in count_dict.keys():
+        sum += count_dict[i]
+        cumulativeSum_dict[i] = sum
+        
+    for index, data in enumerate(cumulativeSum_dict.keys()):
+        if index > 0:
+            if(cumulativeSum_dict[data] == cumulativeSum_dict[data -1]):
+                continue
+            else:
+                count = cumulativeSum_dict[data] - cumulativeSum_dict[data -1]
+                for i in range(count):
+                    sorted.append(data)
+        else:
+            count = cumulativeSum_dict[data]
+            for i in range(count):
+                sorted.append(data)
+    return sorted
+
+if __name__ == '__main__':
+    arr = [7, 9, 3, 2, 3, 1, 5, 1000, 247, 88, 963]
+    min = min(arr)
+    max = max(arr)
+    print(countSort(arr, min, max))
+
+
+
